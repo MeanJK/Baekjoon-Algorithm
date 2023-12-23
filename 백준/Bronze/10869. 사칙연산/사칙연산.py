c0ad722d6316982d2f7b16d9bbea50b6b69a21fa
MJ <122874405+MeanJK@users.noreply.github.com>
2023-12-23 18:05:00 +0900

    [Bronze V] Title: 사칙연산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,7 @@
+Anum, Bnum = map(int, input().split())
+if Anum >= 1 and Bnum <= 10000:
+    print(str(Anum + Bnum))
+    print(str(Anum - Bnum))
+    print(str(Anum * Bnum))
+    print(str(Anum // Bnum))
+    print(str(Anum % Bnum))
