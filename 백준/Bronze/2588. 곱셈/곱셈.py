fa27e262fe44cc77bebe890acd26240c161666a0
MJ <122874405+MeanJK@users.noreply.github.com>
2023-12-24 16:34:13 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,14 @@
+Anum = int(input())
+Bnum = input()
+Cnum = Anum * int(Bnum[2])
+Dnum = Anum * int(Bnum[1])
+Enum = Anum * int(Bnum[0])
+Fnum = Anum * int(Bnum)
+print(Cnum)
+print(Dnum)
+print(Enum)
+print(Fnum)
+        
+        
+      
+  
