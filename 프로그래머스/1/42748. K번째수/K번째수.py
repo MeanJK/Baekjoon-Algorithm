bcc26da48ad9aaea9de8463abd6b3a4f0f5a79e5
MJ <rlaalswls102@naver.com>
2024-02-02 21:53:13 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,12 +1,8 @@
 def solution(array, commands):
-    all_list = []
+    split_list = []
     for i in range(len(commands)):
-        start_idx = commands[i][0] - 1
-        end_idx = commands[i][1]
-        k_idx = commands[i][2]
-        
-        com_list = array[start_idx:end_idx]
-        com_list.sort()
-        all_list.append(com_list[k_idx-1])
-    return all_list
+        split_array = array[commands[i][0]-1:commands[i][1]]
+        split_array.sort()
+        split_list.append(split_array[commands[i][2]-1])
+    return split_list
 print(solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
