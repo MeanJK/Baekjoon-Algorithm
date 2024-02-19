c56319c837987bd97638285c7dbc5e4a930ba5cf
MJ <rlaalswls102@naver.com>
2024-02-19 21:41:42 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,13 @@
+T = input().split('-')
+num = []
+for i in T:
+    sum = 0
+    tmp = i.split('+')
+    for j in tmp:
+        sum+=int(j)
+    num.append(sum)
+
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
