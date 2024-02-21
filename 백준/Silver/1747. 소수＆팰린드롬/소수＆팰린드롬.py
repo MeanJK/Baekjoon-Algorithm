35acd9e65242ff7d0f49dbb2d288153d0b688480
MJ <rlaalswls102@naver.com>
2024-02-21 18:42:44 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 3476 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -0,0 +1,27 @@
+import sys
+import math
+N = int(input())
+min_num = sys.maxsize
+def isPrime(x):
+    for i in range(2,int(math.sqrt(x))+1):
+        if x % i == 0:
+            return False
+    return True
+
+def pelindrom(x):
+    count = 0
+    x = list(str(x))
+    for i in range((len(x)+1)//2):
+        if x[i] == x[(len(x)-1)-i]:
+            count += 1
+    if count == (len(x)+1)//2:
+        return True
+    else:
+        return False
+
+for i in range(N, 10000001):
+    if i == 1:
+        continue
+    if isPrime(i) == True and pelindrom(i) == True:
+        print(i)
+        break
