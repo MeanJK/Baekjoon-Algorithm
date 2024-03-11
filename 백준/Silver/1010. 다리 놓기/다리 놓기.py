21245c0777d55d7435f35b6e3fd89aa225ea6d97
MJ <rlaalswls102@naver.com>
2024-03-11 15:59:22 +0900

    [Silver V] Title: 다리 놓기, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,10 @@
+T = int(input())
+for _ in range(T):
+    n, m = map(int, input().split())
+    result = 1
+    division = 1
+    for i in range(m, m-n, -1):
+        result *= i
+    for i in range(1, n+1):
+        division *= i
+    print(result // division)
